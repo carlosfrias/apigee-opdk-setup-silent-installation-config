5e77b3b58c5e90da03e5e757473e9b47c90200bb
Carlos Frias <cfrias@apigee.com>
2016-10-04 20:27:40 -0400

    updating to fix issue with some of the hostvars as they are created on the rmp nodes

--- library/cassandra_hosts.py
+++ library/cassandra_hosts.py
@@ -87,15 +87,15 @@ def main():
     hostvars = module.params['hostvars']
 
 
-    try:
-        with open(HOSTVARS_FILE, 'w') as hostvars_file:
-            hostvars_file.write(json.dumps(hostvars))
-    except:
-        with open(HOSTVARS_FILE,'r') as hostvars_file:
-            hostvars_data = hostvars_file.read()
-            hostvars_ast = ast.literal_eval(hostvars_data)
-            hostvars_json_dump = json.dumps(hostvars_ast)
-            hostvars = json.loads(hostvars_json_dump)
+    # try:
+    with open(HOSTVARS_FILE, 'w') as hostvars_file:
+        hostvars_file.write(json.dumps(hostvars))
+    # except:
+    #     with open(HOSTVARS_FILE,'r') as hostvars_file:
+    #         hostvars_data = hostvars_file.read()
+    #         hostvars_ast = ast.literal_eval(hostvars_data)
+    #         hostvars_json_dump = json.dumps(hostvars_ast)
+    #         hostvars = json.loads(hostvars_json_dump)
 
     cass_hosts = build_cass_hosts_config(inventory_hostname, hostvars)
     cass_hosts = json.dumps(cass_hosts)

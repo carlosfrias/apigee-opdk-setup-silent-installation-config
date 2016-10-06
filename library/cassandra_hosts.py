8ea5d94ad012f6d7ec787d17f28f7f9645d5196e
Carlos Frias <cfrias@apigee.com>
2016-10-06 11:51:34 -0400

    updated parameter passing to use base64 encoding for complex structure.

--- library/cassandra_hosts.py
+++ library/cassandra_hosts.py
@@ -77,6 +77,7 @@ def main():
             argument_spec=dict(
                     inventory_hostname=dict(required=True, type='str'),
                     hostvars=dict(required=True, type="str"),
+                    _ansible_debug=dict(required=False, type='bool')
                     # public_ip_field_name=dict(required=False, choices=['ec2_ip_address', 'public_address']),
                     # private_ip_field_name=dict(required=False, choices=['ec2_private_ip_address', 'local_address'])
             )
@@ -84,14 +85,15 @@ def main():
     # global SEMANTIC_PRIVATE_ADDRESS, SEMANTIC_PUBLIC_ADDRESS
     # SEMANTIC_PRIVATE_ADDRESS = module.params['private_ip_field_name']
     # SEMANTIC_PUBLIC_ADDRESS = module.params['public_ip_field_name']
-
+    debug = module.params['_ansible_debug']
     inventory_hostname = module.params['inventory_hostname']
     hostvars = module.params['hostvars']
+    hostvars = module.safe_eval(hostvars.decode('base64'))
 
-    with open('hostvars.json','w') as hostvars_file:
-        hostvars_file.write(hostvars)
+    if debug:
+        with open('hostvars.json','w') as hostvars_file:
+            hostvars_file.write(hostvars)
 
-    hostvars = ast.literal_eval(hostvars)
     hostvars = json.dumps(hostvars)
     hostvars = json.loads(hostvars)
 

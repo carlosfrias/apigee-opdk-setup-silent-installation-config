f6c0c7f1bc60dda78445769524a0dd8710e0a275
Carlos Frias <cfrias@apigee.com>
2016-10-14 04:27:16 -0400

    updated exception handling

--- library/cassandra_hosts.py
+++ library/cassandra_hosts.py
@@ -82,30 +82,30 @@ def main():
 
     inventory_hostname = module.params['inventory_hostname']
     hostvars = module.params['hostvars']
-    # with open('hostvars_raw.json', 'w') as hostvars_file:
-    #     hostvars_file.write(hostvars)
+    with open('hostvars_params.json', 'w') as hostvars_file:
+        hostvars_file.write(hostvars)
 
     # hostvars = hostvars.decode('base64')
-    # try:
-    #     hostvars = ast.literal_eval(hostvars)
-    # except SyntaxError as e:
-    #     hostvars = hostvars.replace('{u', '{')
-    #     hostvars = hostvars.replace(", u'", ", '")
-    #     hostvars = hostvars.replace(": u'", ": '")
-    #     hostvars = hostvars.replace("[u'", "['")
-    #     hostvars = hostvars.replace("'", "\"")
-    #     with open('hostvars_raw.json', 'w') as file:
-    #         file.write(hostvars)
-    #
-    #     try:
-    #         hostvars = ast.literal_eval(hostvars)
-    #     except SyntaxError as e:
-    #         msg = "ast.literal_eval conversion failed on line {0} with {1}".format(e.lineno, e.msg)
-    #         module.fail_json(
-    #             changed=False,
-    #             msg=msg,
-    #         )
-    #         return
+    try:
+        hostvars = ast.literal_eval(hostvars)
+    except SyntaxError as e:
+        hostvars = hostvars.replace('{u', '{')
+        hostvars = hostvars.replace(", u'", ", '")
+        hostvars = hostvars.replace(": u'", ": '")
+        hostvars = hostvars.replace("[u'", "['")
+        hostvars = hostvars.replace("'", "\"")
+        with open('hostvars_raw.json', 'w') as file:
+            file.write(hostvars)
+
+        try:
+            hostvars = ast.literal_eval(hostvars)
+        except SyntaxError as e:
+            msg = "ast.literal_eval conversion failed on line {0} with {1}".format(e.lineno, e.msg)
+            module.fail_json(
+                changed=False,
+                msg=msg,
+            )
+            return
 
     # hostvars = json.dumps(hostvars)
     with open('hostvars_dumps.json', 'w') as hostvars_file:
